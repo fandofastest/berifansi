89cf295da6f3637977e5a76ddf5d7c22dd3e5775
Rifando Panggabean <fandofast@gmail.com>
2025-04-10 17:07:40 +0700

    f

--- src/app.js
+++ src/app.js
@@ -34,10 +34,17 @@ const limiter = rateLimit({
 
 // Apply middleware
 app.use(helmet());
+// CORS middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  
   next();
 });
 app.use(express.json());
